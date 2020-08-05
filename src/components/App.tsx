14d4bf202e86f5b6f6b58bb2cbf0ba8b69ae061a
lewohy <lwh8762@gmail.com>
2020-08-06 02:34:09 +0900

    .

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component {
+    public render(): JSX.Element {
+        return (
+            <div>
+                Hello
+            </div>
+        );
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        'index': [ './src/ts/index.tsx', './src/scss/index.scss' ]
+    },
+    devtool: 'inline-source-map',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js'
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.jsx', '.js' ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            }
+        ]
+    }
+};
